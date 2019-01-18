55a96fdc72275c109532cde89145016d37b8e147
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-01-18 14:35:17 +0200

    dom icon defined as a stateless component with forwardable ref

--- dom/icon.js
+++ dom/icon.js
@@ -1,31 +1,20 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import icons from './icons';
 
-class Icon extends PureComponent {
-    render() {
-        return (
-            <svg viewBox={icons[this.props.icon].viewBox}
-                style={this.props.style}
-                className={this.props.className}
-                width={this.props.width}
-                height={this.props.height}
-                fill={this.props.fill}>
-                {icons[this.props.icon].paths.map((path, index) => (
-                    <path key={index} d={path} />
-                ))}
-            </svg>
-        );
-    }
-}
+const Icon = React.forwardRef((props, ref) => (
+    <svg
+        {...props}
+        ref={ref}
+        viewBox={icons[props.icon].viewBox}
+        children={icons[props.icon].paths.map((path, index) => (
+            <path key={index} d={path} />
+        ))}
+    />
+));
 
 Icon.propTypes = {
-    icon: PropTypes.oneOf(Object.keys(icons)).isRequired,
-    style: PropTypes.object,
-    className: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
-    fill: PropTypes.string
+    icon: PropTypes.oneOf(Object.keys(icons)).isRequired
 };
 
 export default Icon;
