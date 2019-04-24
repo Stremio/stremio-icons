37fc28a60feed971002a91915c895aa3e7b94068
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-24 15:22:21 +0300

    scripts moved to scripts folder

--- scripts/generateReadme.js
+++ scripts/generateReadme.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-fs.writeFileSync('./README.md', getReadmeContent());
+fs.writeFileSync('../README.md', getReadmeContent());
 
 function getReadmeContent() {
-    return `|preview|name|
-|:---:|:---:|
-${fs.readdirSync('./preview')
-            .filter((fileName) => fileName.endsWith('.png'))
+    return '|preview|name|' +
+        '|:---:|:---:|' +
+        fs.readdirSync('./preview')
             .map((fileName) => {
                 const iconName = fileName.substring(0, fileName.length - 4);
                 return `|![${iconName}](/preview/${fileName})|${iconName}|`;
             })
-            .join('\n')}`;
+            .join('\n');
 }
