ddf037eef9b5f57e451de4123b7cd65c35cb2b4b
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-06-21 11:35:52 +0300

    updateReadme script generates  preview pngs

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -1,10 +1,28 @@
 const fs = require('fs');
+const svgToImg = require("svg-to-img");
+const { icons, height: iconsHeight } = require('../icons.json');
 
-fs.writeFileSync('../README.md', getReadmeContent());
+Promise.all(icons.map((icon) => {
+    const svg = getSvgContent(icon);
+    return svgToImg.from(svg).toPng({
+        path: `./preview/${icon.properties.name}.png`,
+        height: 32
+    });
+})).then(() => {
+    fs.writeFileSync('./README.md', getReadmeContent());
+});
+
+function getSvgContent(icon) {
+    return (
+        `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${icon.icon.width || iconsHeight} ${iconsHeight}">
+            ${icon.icon.paths.map(d => `<path d="${d}" />`)}
+        </svg>`
+    );
+}
 
 function getReadmeContent() {
-    return '|preview|name|' +
-        '|:---:|:---:|' +
+    return '|preview|name|\n' +
+        '|:---:|:---:|\n' +
         fs.readdirSync('./preview')
             .map((fileName) => {
                 const iconName = fileName.substring(0, fileName.length - 4);
