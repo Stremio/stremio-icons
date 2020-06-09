3b8ad5da6c2869afd75a97c7f5b88c81a01e8444
nklhrstv <me@nikolahristov.com>
2020-06-09 10:18:30 +0300

    ios icon added to native

---
+++ native/Icon.ios.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { View, Image } from 'react-native';
+import icons from '../png';
+
+class Icon extends PureComponent {
+    render() {
+        const { style, icon, color } = this.props;
+        return (
+            <Image
+                style={[style, { tintColor: color }]}
+                source={icons[icon]}
+                resizeMode={'contain'}
+            />
+        );
+    }
+}
+
+Icon.propTypes = {
+    ...View.propTypes,
+    icon: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired
+};
+Icon.defaultProps = {
+    color: '#ccffffff'
+};
+
+export default Icon;
