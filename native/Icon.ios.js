ef90f60389368e19369b71f51225647d777abbf4
nklhrstv <me@nikolahristov.com>
2020-06-10 18:15:56 +0300

    unset width and height in iOS icons

--- native/Icon.ios.js
+++ native/Icon.ios.js
@@ -1,14 +1,21 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { View, Image } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import icons from './png';
+
+const styles = StyleSheet.create({
+    icon: {
+        width: undefined,
+        height: undefined
+    }
+});
 
 class Icon extends PureComponent {
     render() {
         const { style, icon, color } = this.props;
         return (
             <Image
-                style={[style, { tintColor: color }]}
+                style={[styles.icon, style, { tintColor: color }]}
                 source={icons[icon]}
                 resizeMode={'contain'}
             />
