eaffe8bd978dea6aa02836bd9f981e138a2ddb90
Tim <tymmesyde@gmail.com>
2023-01-30 15:43:21 +0100

    use svg file instead of a IcoMoon zip

--- build.js
+++ build.js
@@ -1,118 +1,67 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
+const path = require('path');
+const { xml2js } = require('xml-js');
+const svgPath = require('svg-path');
+const { camelCase } = require('change-case');
 
-function toSVG(icon) {
-    return `<svg viewBox="0 0 ${icon.icon.width || icons.height} ${icons.height}">${icon.icon.paths.map((d) => `<path d="${d}" />`)}</svg>`;
+const findSVGElement = (object, id) => {
+    if (object?.attributes?.id === id) {
+        return object;
+    } else {
+        if (object.elements) {
+            for (const element of object.elements) {
+                const found = findSVGElement(element, id);
+                if (found) {
+                    return found;
+                }
+            }
+        }
+    }
+    return null;
 }
 
-function toDrawable(icon) {
-    return `<?xml version="1.0" encoding="utf-8"?>
-<vector xmlns:android="http://schemas.android.com/apk/res/android"
-\tandroid:width="${(icon.icon.width || icons.height) / 20}dp"
-\tandroid:height="${icons.height / 20}dp"
-\tandroid:viewportWidth="${icon.icon.width || icons.height}"
-\tandroid:viewportHeight="${icons.height}">
-${icon.icon.paths.map((path) => `\t<path
-\t\tandroid:fillColor="#ffffffff"
-\t\tandroid:pathData="${path}" />`).join('\n')}
-</vector>`;
-}
+const getStyleFromAttributes = (attributes) => {
+    const strokeAttribtutes = ['stroke-width', 'stroke-miterlimit', 'stroke-linejoin', 'stroke-linecap'];
+    const style = Object.fromEntries(Object.entries(attributes)
+        .filter(([key]) => strokeAttribtutes.includes(key))
+        .map(([key, value]) => [camelCase(key), value]));
 
-execSync(`unzip icons.zip -d icons`);
+    if (Object.keys(style).find((key) => key.startsWith('stroke'))) {
+        style['fill'] = 'none';
+    }
 
-const icons = JSON.parse(fs.readFileSync('./icons/selection.json'));
+    return style;
+};
 
-if (process.argv.includes('all') || process.argv.includes('docs')) {
-    mkdirp.sync('docs');
-    fs.writeFileSync(
-        'docs/index.md',
-        `---
-layout: default
-title: Stremio Icons
----
+const svgIcons = fs.readFileSync(path.join(__dirname, 'stremio-icons.svg'));
+const parsedSVG = xml2js(svgIcons);
 
-|preview|name|
-|:---:|:---:|\n`
-        +
-        icons.icons.map((icon) => {
-            return `|![${icon.properties.name}](${icon.properties.name}.png)|${icon.properties.name}|`;
-        }).join('\n')
-    );
-    icons.icons.forEach((icon) => {
-        const svg = toSVG(icon);
-        fs.writeFileSync('tmp.svg', svg);
-        execSync(`inkscape -z -w ${(icon.icon.width || icons.height) / 30} -h ${icons.height / 30} tmp.svg -e docs/${icon.properties.name}.png`);
-        rimraf.sync('tmp.svg');
+const stremioIcons = findSVGElement(parsedSVG, 'stremio-icons');
+const icons = Object.fromEntries(stremioIcons.elements.filter(({ name }) => name === 'g').map((icon) => {
+    const iconOuter = icon.elements.find(({ name }) => name === 'rect');
+    const iconInner = icon.elements.find(({ name }) => name !== 'rect');
+
+    const viewBox = `0 0 ${iconOuter.attributes.width} ${iconOuter.attributes.height}`;
+    const paths = iconInner.elements.map(({ attributes }) => {
+        const style = getStyleFromAttributes(attributes);
+
+        const path = svgPath(attributes.d);
+        path.translate(-iconOuter.attributes.x | 0, -iconOuter.attributes.y | 0);
+        const d = path.toString();
+
+        return {
+            d,
+            style
+        };
     });
-}
 
-if (process.argv.includes('all') || process.argv.includes('fonts')) {
-    mkdirp.sync('fonts');
-    fs.copyFileSync('icons/fonts/stremio-icons.svg', 'fonts/stremio-icons.svg');
-    fs.copyFileSync('icons/fonts/stremio-icons.ttf', 'fonts/stremio-icons.ttf');
-    fs.copyFileSync('icons/fonts/stremio-icons.woff', 'fonts/stremio-icons.woff');
-}
+    return [
+        icon.attributes.id,
+        {
+            viewBox,
+            paths
+        }
+    ];
+}));
 
-if (process.argv.includes('all') || process.argv.includes('css')) {
-    mkdirp.sync('css');
-    fs.copyFileSync('icons/style.css', 'css/icons.css');
-}
-
-if (process.argv.includes('all') || process.argv.includes('android')) {
-    mkdirp.sync('android/src/main/res/drawable');
-    icons.icons.forEach((icon) => {
-        const drawable = toDrawable(icon);
-        fs.writeFileSync(
-            `android/src/main/res/drawable/${icon.properties.name}.xml`,
-            drawable
-        );
-    });
-}
-
-if (process.argv.includes('all') || process.argv.includes('react-native')) {
-    mkdirp.sync('react-native/ios/png');
-    icons.icons.forEach((icon) => {
-        const svg = toSVG(icon);
-        fs.writeFileSync('tmp.svg', svg);
-        execSync(`inkscape -z -w ${(icon.icon.width || icons.height) / 16} -h ${icons.height / 16} tmp.svg -e react-native/ios/png/${icon.properties.name}.png`);
-        rimraf.sync('tmp.svg');
-    });
-    fs.writeFileSync(
-        'react-native/ios/png/index.js',
-        icons.icons.map((icon) => {
-            return `var ${icon.properties.name} = require('./${icon.properties.name}.png');`;
-        }).join('\n')
-        +
-        '\nmodule.exports = {\n'
-        +
-        icons.icons.map((icon) => icon.properties.name).join(',\n')
-        +
-        '\n};'
-    );
-
-    mkdirp.sync('react-native/android/src/main/res/drawable');
-    icons.icons.forEach((icon) => {
-        const drawable = toDrawable(icon);
-        fs.writeFileSync(
-            `react-native/android/src/main/res/drawable/${icon.properties.name}.xml`,
-            drawable
-        );
-    });
-}
-
-if (process.argv.includes('all') || process.argv.includes('dom')) {
-    fs.writeFileSync(
-        'dom/icons.json',
-        JSON.stringify(icons.icons.reduce((result, icon) => {
-            result[icon.properties.name] = {
-                viewBox: `0 0 ${icon.icon.width || icons.height} ${icons.height}`,
-                paths: icon.icon.paths
-            };
-            return result;
-        }, {}))
-    );
-}
-
-rimraf.sync('icons');
+fs.writeFileSync('dom/icons.json', JSON.stringify(icons));
--- dom/Icon.js
+++ dom/Icon.js
@@ -3,14 +3,14 @@ var PropTypes = require('prop-types');
 var icons = require('./icons.json');
 
 var Icon = React.forwardRef(function(props, ref) {
-    var icon = icons[props.icon];
+    var icon = icons[props.name];
     if (!icon) {
         return null;
     }
 
     return React.createElement('svg', Object.assign({ viewBox: icon.viewBox }, props, { ref: ref }),
         icon.paths.map(function(path, index) {
-            return React.createElement('path', { key: index, d: path });
+            return React.createElement('path', { key: index, d: path.d, style: path.style });
         })
     );
 });
