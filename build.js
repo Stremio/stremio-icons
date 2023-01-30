b48fcba9e3097b8337759381b5f6ea8640de0a2a
Tim <tymmesyde@gmail.com>
2023-01-30 18:20:46 +0100

    build: generate doc

--- build.js
+++ build.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { xml2js } = require('xml-js');
 const svgPath = require('svg-path');
-const { camelCase } = require('change-case');
+const { camelCase, paramCase } = require('change-case');
+const sharp = require('sharp');
 
 const findSVGElement = (object, id) => {
     if (object?.attributes?.id === id) {
@@ -20,30 +21,23 @@ const findSVGElement = (object, id) => {
     return null;
 }
 
-const getStyleFromAttributes = (attributes) => {
-    const strokeAttribtutes = ['stroke-width', 'stroke-miterlimit', 'stroke-linejoin', 'stroke-linecap'];
-    const style = Object.fromEntries(Object.entries(attributes)
-        .filter(([key]) => strokeAttribtutes.includes(key))
-        .map(([key, value]) => [camelCase(key), value]));
-
-    if (Object.keys(style).find((key) => key.startsWith('stroke'))) {
-        style['fill'] = 'none';
-    }
-
-    return style;
-};
-
 const svgIcons = fs.readFileSync(path.join(__dirname, 'stremio-icons.svg'));
 const parsedSVG = xml2js(svgIcons);
 
 const stremioIcons = findSVGElement(parsedSVG, 'stremio-icons');
-const icons = Object.fromEntries(stremioIcons.elements.filter(({ name }) => name === 'g').map((icon) => {
+
+const icons = stremioIcons.elements.filter(({ name }) => name === 'g').map((icon) => {
     const iconOuter = icon.elements.find(({ name }) => name === 'rect');
     const iconInner = icon.elements.find(({ name }) => name !== 'rect');
 
     const viewBox = `0 0 ${iconOuter.attributes.width} ${iconOuter.attributes.height}`;
     const paths = iconInner.elements.map(({ attributes }) => {
-        const style = getStyleFromAttributes(attributes);
+        const ignoreAttributes = ['id', 'd', 'fill', 'stroke'];
+        const styles = Object.fromEntries(Object.entries(attributes).filter(([key]) => !ignoreAttributes.includes(key)));
+
+        if (Object.keys(styles).some((key) => key === 'stroke-width')) {
+            styles['fill'] = 'none';
+        }
 
         const path = svgPath(attributes.d);
         path.translate(-iconOuter.attributes.x | 0, -iconOuter.attributes.y | 0);
@@ -51,17 +45,86 @@ const icons = Object.fromEntries(stremioIcons.elements.filter(({ name }) => name
 
         return {
             d,
-            style
+            styles
         };
     });
 
-    return [
-        icon.attributes.id,
-        {
-            viewBox,
-            paths
-        }
-    ];
-}));
+    return {
+        name: icon.attributes.id,
+        viewBox,
+        paths,
+    }
+});
 
-fs.writeFileSync('dom/icons.json', JSON.stringify(icons));
+if (process.argv.includes('all') || process.argv.includes('dom')) {
+    const styleAttribtutes = ['stroke-width', 'stroke-miterlimit', 'stroke-linejoin', 'stroke-linecap', 'fill'];
+
+    const toReactStyle = (styles) => {
+        return Object.fromEntries(Object.entries(styles)
+            .filter(([key]) => styleAttribtutes.includes(key))
+            .map(([key, value]) => [camelCase(key), value]));
+    }
+
+    const JSONIcons = Object.fromEntries(icons.map(({ name, viewBox, paths }) => {
+        const reactPaths = paths.map(({ d, styles }) => ({
+            d,
+            style: toReactStyle(styles)
+        }));
+        
+        return [
+            name,
+            {
+                viewBox,
+                paths: reactPaths
+            }
+        ]
+    }));
+
+    fs.writeFileSync('dom/icons.json', JSON.stringify(JSONIcons));
+}
+
+if (process.argv.includes('all') || process.argv.includes('docs')) {
+    const toInlineBlackStyle = (style) => {
+        const styleEntries = Object.entries(style);
+        if (styleEntries.some(([key]) => key === 'stroke-width')) {
+            styleEntries.push(['stroke', '#000000']);
+        }
+
+        return styleEntries
+            .map(([key, value]) => `${paramCase(key)}:${value}`)
+            .join(';');
+    };
+
+    const toSVGFile = (viewBox, paths) => {
+        const svgPaths = paths
+            .map(({ d, styles }) => `<path d="${d}" style="${toInlineBlackStyle(styles)}" />`)
+            .join('');
+        return `<svg viewBox="${viewBox}">${svgPaths}</svg>`;
+    };
+
+    (async () => {
+        const pngFiles = await Promise.all(icons.map(async ({ name, viewBox, paths }) => {
+            const svgBuffer = Buffer.from(toSVGFile(viewBox, paths));
+            const pngBuffer = await sharp(svgBuffer).png().resize(32, 32).toBuffer();
+            return {
+                name,
+                buffer: pngBuffer
+            };
+        }));
+        
+        pngFiles.forEach(({ name, buffer }) => {
+            fs.writeFileSync(path.join(__dirname, 'docs', `${name}.png`), buffer);
+        });
+    })();
+
+    const markdownIndex = `---
+layout: default
+title: Stremio Icons
+---
+|preview|name|
+|:---:|:---:|\n`
+        +
+        icons.map(({ name }) => `|![${name}](${name}.png)|${name}|`).join('\n');
+
+    fs.writeFileSync('docs/index.md', markdownIndex);
+}
