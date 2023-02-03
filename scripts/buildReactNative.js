0e6d09be94f7aa2c69d35da12c905f0396e05eb0
Tim <tymmesyde@gmail.com>
2023-02-03 07:21:15 +0100

    build: clean code

--- scripts/buildReactNative.js
+++ scripts/buildReactNative.js
@@ -2,37 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const { toPngFiles, toDrawableFiles, removeDir } = require('./utils');
 
+const buildIOS = async (cwd, icons) => {
+    const iosResourcesPath = path.join(cwd, 'ios', 'png');
+    removeDir(iosResourcesPath);
+    fs.mkdirSync(iosResourcesPath, { recursive: true });
+
+    const pngFiles = await toPngFiles(icons, 64);
+    pngFiles.forEach(({ filename, buffer }) => {
+        fs.writeFileSync(path.join(iosResourcesPath, filename), buffer);
+    });
+
+    const pngFilesJSIndex = [
+        'module.exports = {',
+            pngFiles.map(({ name }) => `['${name}']: require('./${name}.png')`).join(',\n'),
+        '};'
+    ].join('\n');
+
+    fs.writeFileSync(path.join(iosResourcesPath, 'index.js'), pngFilesJSIndex);
+};
+
+const buildAndroid = async (cwd, icons) => {
+    const androidResourcesPath = path.join(cwd, 'android', 'src', 'main', 'res', 'drawable');
+    removeDir(androidResourcesPath);
+    fs.mkdirSync(androidResourcesPath, { recursive: true });
+    
+    const drawableFiles = await toDrawableFiles(icons);
+    drawableFiles.forEach(({ filename, buffer }) => {
+        fs.writeFileSync(path.join(androidResourcesPath, filename), buffer)
+    });
+};
+
 const buildReactNative = (icons) => {
     (async () => {
         const reactNativePath = path.join(process.cwd(), 'react-native');
-        const iosResourcesPath = path.join(reactNativePath, 'ios', 'png');
-        const androidResourcesPath = path.join(reactNativePath, 'android', 'src', 'main', 'res', 'drawable');
-
-        removeDir(iosResourcesPath);
-        removeDir(androidResourcesPath);
-
-        fs.mkdirSync(iosResourcesPath, { recursive: true });
-        fs.mkdirSync(androidResourcesPath, { recursive: true });
-
-        // iOS
-        const pngFiles = await toPngFiles(icons, 64);
-        pngFiles.forEach(({ filename, buffer }) => {
-            fs.writeFileSync(path.join(iosResourcesPath, filename), buffer);
-        });
-
-        const pngFilesJSIndex = [
-            'module.exports = {',
-                pngFiles.map(({ name }) => `['${name}']: require('./${name}.png')`).join(',\n'),
-            '};'
-        ].join('\n');
-
-        fs.writeFileSync(path.join(iosResourcesPath, 'index.js'), pngFilesJSIndex);
-
-        // Android
-        const drawableFiles = await toDrawableFiles(icons);
-        drawableFiles.forEach(({ filename, buffer }) => {
-            fs.writeFileSync(path.join(androidResourcesPath, filename), buffer)
-        });
+        await buildIOS(reactNativePath, icons)
+        await buildAndroid(reactNativePath, icons);
     })();
 };
 
