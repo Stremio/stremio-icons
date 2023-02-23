46244fc9e540f956bbc9819c6910dbec96a472ba
Tim <tymmesyde@gmail.com>
2023-02-23 15:12:40 +0100

    build: optimize svg paths

--- scripts/build.js
+++ scripts/build.js
@@ -3,19 +3,21 @@ const path = require('path');
 const { xml2js } = require('xml-js');
 const svgPath = require('svg-path');
 const { parseSVG } = require('svg-path-parser');
+const { optimize } = require('svgo');
+const { toSVGFile } = require('./utils');
 const buildSolid = require('./buildSolid');
 const buildReact = require('./buildReact');
 const buildReactNative = require('./buildReactNative');
 const buildAndroid = require('./buildAndroid');
 const buildDocs = require('./buildDocs');
 
-const findSVGElement = (object, id) => {
-    if (object?.attributes?.id === id) {
+const findSVGElement = (object, options) => {
+    if ((options.id && object?.attributes?.id === options.id) || (options.name && object?.name === options.name)) {
         return object;
     } else {
         if (object.elements) {
             for (const element of object.elements) {
-                const found = findSVGElement(element, id);
+                const found = findSVGElement(element, options);
                 if (found) {
                     return found;
                 }
@@ -28,7 +30,7 @@ const findSVGElement = (object, id) => {
 const svgIcons = fs.readFileSync(path.join(process.cwd(), 'stremio-icons.svg'));
 const parsedSVG = xml2js(svgIcons);
 
-const stremioIcons = findSVGElement(parsedSVG, 'stremio-icons');
+const stremioIcons = findSVGElement(parsedSVG, { id: 'stremio-icons' });
 
 const ICON_SIZE = 512;
 
@@ -46,9 +48,8 @@ const icons = stremioIcons.elements.filter(({ name }) => name === 'g').map((icon
     const viewBox = `0 0 ${ICON_SIZE} ${ICON_SIZE}`;
 
     const paths = iconInner.elements.map(({ attributes }) => {
-        const ignoreAttributes = ['id', 'd', 'fill', 'stroke'];
+        const ignoreAttributes = ['id', 'd'];
         const styles = Object.fromEntries(Object.entries(attributes).filter(([key]) => !ignoreAttributes.includes(key)));
-
         if (Object.keys(styles).some((key) => key === 'stroke-width')) {
             styles['fill'] = 'none';
         }
@@ -63,12 +64,25 @@ const icons = stremioIcons.elements.filter(({ name }) => name === 'g').map((icon
         };
     });
 
+    const svg = toSVGFile(viewBox, paths);
+    const { data } = optimize(svg);
+    const parsedSVG = xml2js(data);
+    const root = findSVGElement(parsedSVG, { name: 'svg' });
+    const optimizedPaths = root.elements.map(({ attributes }) => {
+        const { d, style } = attributes;
+        const styles = style ? Object.fromEntries(style.split(';').map((s) => s.split(':'))) : {};
+        return {
+            d,
+            styles
+        };
+    });
+
     return {
         name: icon.attributes.id,
         width: ICON_SIZE,
         height: ICON_SIZE,
         viewBox,
-        paths,
+        paths: optimizedPaths,
     }
 });
 
--- scripts/utils.js
+++ scripts/utils.js
@@ -4,25 +4,20 @@ const { paramCase, camelCase, snakeCase } = require('change-case');
 const sharp = require('sharp');
 const glob = require('glob');
 
-const toPngFiles = (icons, size) => {
-    const toInlineBlackStyle = (style) => {
-        const styleEntries = Object.entries(style);
-        if (styleEntries.some(([key]) => key === 'stroke-width')) {
-            styleEntries.push(['stroke', '#000000']);
-        }
-
-        return styleEntries
+const toSVGFile = (viewBox, paths) => {
+    const toInlineStyle = (style) => {
+        return Object.entries(style)
             .map(([key, value]) => `${paramCase(key)}:${value}`)
             .join(';');
     };
 
-    const toSVGFile = (viewBox, paths) => {
-        const svgPaths = paths
-            .map(({ d, styles }) => `<path d="${d}" style="${toInlineBlackStyle(styles)}" />`)
-            .join('');
-        return `<svg viewBox="${viewBox}">${svgPaths}</svg>`;
-    };
+    const svgPaths = paths
+        .map(({ d, styles }) => `<path d="${d}" style="${toInlineStyle(styles)}" />`)
+        .join('');
+    return `<svg viewBox="${viewBox}">${svgPaths}</svg>`;
+};
 
+const toPngFiles = (icons, size) => {
     return Promise.all(icons.map(async ({ name, viewBox, paths }) => {
         const svgBuffer = Buffer.from(toSVGFile(viewBox, paths));
         const pngBuffer = await sharp(svgBuffer).png().resize(size, size).toBuffer();
@@ -107,6 +102,7 @@ const removeDir = (path) => {
 };
 
 module.exports = {
+    toSVGFile,
     toPngFiles,
     toDrawableFiles,
     removeFiles,
