0da0843f998e4eee10264db1e4a6e2ca07d10500
Tim <tymmesyde@gmail.com>
2023-05-06 12:03:52 +0200

    build(angularjs): use babel with webpack

--- angularjs/webpack.config.js
+++ angularjs/webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    }
 };
