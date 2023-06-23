5a6954140ab93541efeb8a21d73802aac877bca9
Sleeyax <yourd3veloper@gmail.com>
2023-06-23 11:09:33 +0200

    build: specify compileOptions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -42,9 +42,16 @@ kotlin {
 }
 
 android {
+    compileSdk = 33
+
     defaultConfig {
         minSdk = 22
         compileSdk = 33
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     sourceSets {
