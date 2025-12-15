20673c93d8d982cf387662871ef824bb8e7d6f82
Tim <tymmesyde@gmail.com>
2025-12-15 10:01:59 +0100

    5.9.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,7 +30,7 @@ afterEvaluate {
 
                 groupId = "com.github.Stremio"
                 artifactId = "stremio-icons"
-                version = "5.8.0"
+                version = "5.9.0"
             }
         }
     }
