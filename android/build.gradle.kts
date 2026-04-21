4c864166739a6ead6fc813014f16c8c16b6ca954
Timothy Z. <timothy@stremio.com>
2026-04-21 17:19:37 +0300

    bump: release v5.10.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,7 +30,7 @@ afterEvaluate {
 
                 groupId = "com.github.Stremio"
                 artifactId = "stremio-icons"
-                version = "5.9.0"
+                version = "5.10.0"
             }
         }
     }
